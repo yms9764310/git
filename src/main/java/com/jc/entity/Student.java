858d96de96145231f91101e3f83eed538a3b006a
YMS <739070594@qq.com>
2019-07-08 16:43:45 +0800

    Initial commit

---
+++ src/main/java/com/jc/entity/Student.java
@@ -0,0 +1,18 @@
+package com.jc.entity;
+
+/**
+ * 年: 2019
+ * 月: 07
+ * 日: 08
+ * 小时: 16
+ * 分钟: 40
+ *
+ * @author 严脱兔
+ */
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("第一个Git测试");
+        System.out.println("第一个Git测试");
+        System.out.println("第一个Git测试");
+    }
+}
